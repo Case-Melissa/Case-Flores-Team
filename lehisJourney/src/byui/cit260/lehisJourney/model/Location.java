56a5229bcfcad165faabd21c67e131524187be5a
Sylvia <Sylvia@10.0.0.14>
2016-02-01 23:22:17 -0700

    01/28/2016 Item Committed

---
+++ lehisJourney/src/byui/cit260/lehisJourney/model/Location.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lehisJourney.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Sylvia
+ */
+public class Location implements Serializable{
+    
+    // class instance variables
+    // class instance variables
+    private String row;
+    private String description;
+    private String column;
+    private Item item;
+    private Map map;
+    private Character character;
+    private LabansChallengeSceneType labansChallengeSceneType;
+
+    public Location() {
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    public LabansChallengeSceneType getLabansChallengeSceneType() {
+        return labansChallengeSceneType;
+    }
+
+    public void setLabansChallengeSceneType(LabansChallengeSceneType labansChallengeSceneType) {
+        this.labansChallengeSceneType = labansChallengeSceneType;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "row=" + row + ", description=" + description + ", column=" + column + ", item=" + item + ", map=" + map + ", character=" + character + ", labansChallengeSceneType=" + labansChallengeSceneType + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.row);
+        hash = 23 * hash + Objects.hashCode(this.description);
+        hash = 23 * hash + Objects.hashCode(this.column);
+        hash = 23 * hash + Objects.hashCode(this.item);
+        hash = 23 * hash + Objects.hashCode(this.map);
+        hash = 23 * hash + Objects.hashCode(this.character);
+        hash = 23 * hash + Objects.hashCode(this.labansChallengeSceneType);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (!Objects.equals(this.row, other.row)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.column, other.column)) {
+            return false;
+        }
+        if (!Objects.equals(this.item, other.item)) {
+            return false;
+        }
+        if (!Objects.equals(this.map, other.map)) {
+            return false;
+        }
+        if (!Objects.equals(this.character, other.character)) {
+            return false;
+        }
+        return Objects.equals(this.labansChallengeSceneType, other.labansChallengeSceneType);
+    }
+
+    
+}
