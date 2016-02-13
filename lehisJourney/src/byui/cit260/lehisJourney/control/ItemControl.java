d62f93d80dc2bdbfca8b63b84bea852008c3b1ea
Sylvia <Sylvia@10.0.0.14>
2016-02-13 16:14:14 -0700

    01/28/2016 Item Committed

--- lehisJourney/src/byui/cit260/lehisJourney/control/ItemControl.java
+++ lehisJourney/src/byui/cit260/lehisJourney/control/ItemControl.java
@@ -32,14 +32,15 @@ public class ItemControl {
             return platesFit;
     
 }
+      
     public double calcLabansChallenge(double height, double diameter){
     
     if (height < 36 || height > 48){
     return -1;
-}
+    }
     if (diameter < 24 || diameter > 30){
     return -1;
-}
+    }
     double r = diameter / 2;
     double volume = (Math.PI * r * r * height);
     
@@ -47,5 +48,33 @@ public class ItemControl {
     }
         return volume;
 }
+    
+    public double calcAreaTentRoom(double length, double width, double height, int peopleInRoom){
+   
+        if (height < 8 || height > 8) { // height is incorrect
+            return -1;
+        }
+        if (width < 16 || width > 22){ // width is incorrect
+            return -1;
+        }
+        if (length < 16 || length > 22){ // length is incorrect
+            return -1;
+        }
+        if (peopleInRoom <2 || peopleInRoom >4) { // number of people in the room is incorrect
+                return -1;
+        }
+        double area = length * width;
+        
+        double numOfRoom = 4;
+        
+	double areaPeopleInRoom = (area/4)/peopleInRoom;
+        
+        if (peopleInRoom >= 2 || peopleInRoom <= 4){
+            return area;   
+        }
+        
+        return areaPeopleInRoom;
+
+    }
+}  
   
-}
