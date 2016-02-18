d20230a3509669c903f667b180aad176b5d75a3b
Sylvia <Sylvia@10.0.0.14>
2016-02-17 20:26:15 -0700

    01/28/2016 Item Committed

--- lehisJourney/src/byui/cit260/lehisJourney/control/ItemControl.java
+++ lehisJourney/src/byui/cit260/lehisJourney/control/ItemControl.java
@@ -10,71 +10,64 @@ package byui.cit260.lehisJourney.control;
  * @author Melissa Case
  */
 public class ItemControl {
- 
-     public double calcBasketVolume(double height, double width, double length){
-         
+
+    public double calcBasketVolume(double height, double width, double length) {
+
         if (height < 8 || height > 36) { // height is incorrect
-          return -1;
-     }
-        if (width < 12 || width > 24){
-         return -1;
-     }
-        if (length < 12 || length > 36){
-          return -1;
-         
-     }
+            return -1;
+        }
+        if (width < 12 || width > 24) {
+            return -1;
+        }
+        if (length < 12 || length > 36) {
+            return -1;
+
+        }
         double plates = 6912;
         double volume = (length * width * height);
         double platesFit = (plates / volume);
-        
-        if (platesFit <=4.5){        
+
+        if (platesFit <= 4.5) {
         }
-            return platesFit;
-    
-}
-      
-    public double calcLabansChallenge(double height, double diameter){
-    
-    if (height < 36 || height > 48){
-    return -1;
+        return platesFit;
+
     }
-    if (diameter < 24 || diameter > 30){
-    return -1;
-    }
-    double r = diameter / 2;
-    double volume = (Math.PI * r * r * height);
-    
-    if (volume == 25861.59){
-    }
+
+    public double calcLabansChallenge(double height, double diameter) {
+
+        if (height < 36 || height > 48) {
+            return -1;
+        }
+        if (diameter < 24 || diameter > 30) {
+            return -1;
+        }
+        double r = diameter / 2;
+        double volume = (Math.PI * r * r * height);
+
+        if (volume == 25861.59) {
+        }
         return volume;
-}
-    
-    public double calcAreaTentRoom(double length, double width, double height, int peopleInRoom){
-   
+    }
+
+    public double calcAreaTentRoom(double length, double width, double height, int peopleInRoom) {
+
         if (height < 8 || height > 8) { // height is incorrect
             return -1;
         }
-        if (width < 16 || width > 22){ // width is incorrect
+        if (width < 16 || width > 22) { // width is incorrect
             return -1;
         }
-        if (length < 16 || length > 22){ // length is incorrect
+        if (length < 16 || length > 22) { // length is incorrect
             return -1;
         }
-        if (peopleInRoom <2 || peopleInRoom >4) { // number of people in the room is incorrect
-                return -1;
+        if (peopleInRoom < 2 || peopleInRoom > 4) { // number of people in the room is incorrect
+            return -1;
         }
         double area = length * width;
-        
-        double numOfRoom = 4;
-        
-	double areaPeopleInRoom = (area/4)/peopleInRoom;
-        
-        if (peopleInRoom >= 2 || peopleInRoom <= 4){
-            return area;   
-        }
-        
+
+        double areaPeopleInRoom = (area / peopleInRoom);
+
         return areaPeopleInRoom;
 
     }
-}  
-  
+}
