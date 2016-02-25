00131345b5679d2145670d0d30c332c572482a89
Sylvia <Sylvia@10.0.0.14>
2016-02-24 23:31:13 -0700

    01/28/2016 Item Committed

--- lehisJourney/src/byui/cit260/lehisJourney/view/CollectingFoodWaterView.java
+++ lehisJourney/src/byui/cit260/lehisJourney/view/CollectingFoodWaterView.java
@@ -5,82 +5,105 @@
  */
 package byui.cit260.lehisJourney.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Sylvia
  */
 public class CollectingFoodWaterView {
-   
+
     private final String menu = "\n"
-          + "\nB - Look for Berries"
-          + "\nH - Seek for Honey"
-          + "\nE - Find some Wheat"
-          + "\nW - Search for Water"
-          + "\nU - Look for Unknown plants"
-          + "\nQ - Quit";
-          
-          public CollectingFoodWaterView(){
-         
-      }
-          
-       public void  displayMenu(){   
-           
-           char selection = ' ';
-           
-           do{
-              System.out.println(menu);
-              
+            + "\nB - Look for Berries"
+            + "\nH - Seek for Honey"
+            + "\nE - Find some Wheat"
+            + "\nW - Search for Water"
+            + "\nU - Look for Unknown plants"
+            + "\nQ - Quit";
+
+    public CollectingFoodWaterView() {
+
+    }
+
+    public void displayMenu() {
+
+        char selection = ' ';
+
+        do {
+            System.out.println(menu);
+
 //             String input = getInput();
- //            selection = input.charAt(0);
-             
-             doAction(selection);
-             
-           } while(selection != 'Q');
-}
-       public void doAction(char selection){
-           
-           switch(selection){
-               case 'B':
-                  lookBerries();
-                   break;
-               case 'H':
-                   seekHoney(); 
-                    break;
-                case 'E':
-                    findWheat();
-                    break;
-                case 'W':
-                    searchWater();
-                    break;
-                case 'U':
-                    lookUnknownPlants();
-                    break;
-                case 'Q':
-                    break;
-                default:
-                    System.out.println("Invalid option");
-                    break; 
-           }
+//            selection = input.charAt(0);
+            doAction(selection);
+
+        } while (selection != 'Q');
+    }
+
+    public void doAction(char selection) {
+
+        switch (selection) {
+            case 'B':
+                lookBerries();
+                break;
+            case 'H':
+                seekHoney();
+                break;
+            case 'E':
+                findWheat();
+                break;
+            case 'W':
+                searchWater();
+                break;
+            case 'U':
+                lookUnknownPlants();
+                break;
+            case 'Q':
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+        }
+    }
+
+    public String getInput() {
+        Scanner keyboard = new Scanner(System.in);
+        String input = null;
+        boolean isValid = false;
+
+        while (!isValid) {
+            System.out.println("Please select an option: ");
+            input = keyboard.nextLine();
+            input = input.trim();
+
+            if (input == null || input.length() == 0) {
+                System.out.println("Invalid input - please enter a valid character.");
+            } else {
+                isValid = true;
+            }
+        }
+
+        return input.toUpperCase();
+
     }
 
     private void lookBerries() {
-        System.out.println ("Look for Berries");
+        System.out.println("Look for Berries");
     }
 
     private void seekHoney() {
-       System.out.println ("Seek for Honey");
-    } 
+        System.out.println("Seek for Honey");
+    }
 
     private void findWheat() {
-        System.out.println ("Find some Wheat");
+        System.out.println("Find some Wheat");
     }
 
     private void searchWater() {
-        System.out.println ("Search for Water");
+        System.out.println("Search for Water");
     }
 
     private void lookUnknownPlants() {
-        System.out.println ("Look for Unknown Plants");
+        System.out.println("Look for Unknown Plants");
     }
-       
+
 }
