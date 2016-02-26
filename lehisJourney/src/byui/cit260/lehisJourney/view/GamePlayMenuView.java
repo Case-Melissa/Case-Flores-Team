88c89bd33e11ecc4687721e23f1785015fd2b565
Melissa Case <Melissa Case@LAPTOP-J102H57U>
2016-02-26 07:37:51 -0800

    Merge origin/master
    Conflicts:
    	lehisJourney/src/byui/cit260/lehisJourney/control/ProgramController.java
    	lehisJourney/src/byui/cit260/lehisJourney/model/Game.java
    	lehisJourney/src/byui/cit260/lehisJourney/view/MainMenuView.java
    	lehisJourney/src/byui/cit260/lehisJourney/view/helpMenuView.java

--- lehisJourney/src/byui/cit260/lehisJourney/view/GamePlayMenuView.java
+++ lehisJourney/src/byui/cit260/lehisJourney/view/GamePlayMenuView.java
@@ -64,7 +64,7 @@ public String getInput(){
        String input = null;
        boolean isValid = false;
        
-       while(isValid){
+       while(!isValid){
            System.out.println("Please select an option: ");
            input =  keyboard.nextLine();
            input = input.trim();
