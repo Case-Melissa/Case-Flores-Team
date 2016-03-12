2664113585d608c8c234a5ddc7fd7b4906cd7da7
Sylvia <Sylvia@10.0.0.14>
2016-03-11 21:18:52 -0700

    Merge origin/master
    Conflicts:
    	lehisJourney/src/byui/cit260/lehisJourney/control/ItemControl.java

--- lehisJourney/src/byui/cit260/lehisJourney/control/MapControl.java
+++ lehisJourney/src/byui/cit260/lehisJourney/control/MapControl.java
@@ -21,5 +21,13 @@ public class MapControl {
     assignScenesToLocations(map, scenes);
     return map;
     }
+
+    private static Scene[] createScenes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void assignScenesToLocations(Map map, Scene[] scenes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
