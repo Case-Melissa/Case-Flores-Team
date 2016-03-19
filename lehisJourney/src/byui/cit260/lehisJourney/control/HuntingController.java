0eb1690026642ea914c357ed7baa6cfdb3845fd9
Melissa Case <Melissa Case@LAPTOP-J102H57U>
2016-03-18 21:11:04 -0700

    Merge origin/master
    Conflicts:
    	lehisJourney/src/byui/cit260/lehisJourney/control/ProgramController.java
    	lehisJourney/src/byui/cit260/lehisJourney/model/Game.java
    	lehisJourney/src/byui/cit260/lehisJourney/view/MainMenuView.java
    	lehisJourney/src/byui/cit260/lehisJourney/view/helpMenuView.java

--- lehisJourney/src/byui/cit260/lehisJourney/control/HuntingController.java
+++ lehisJourney/src/byui/cit260/lehisJourney/control/HuntingController.java
@@ -5,25 +5,60 @@
  */
 package byui.cit260.lehisJourney.control;
 
+import byui.cit260.lehisJourney.exceptions.HuntingControllerException;
+import byui.cit260.lehisJourney.model.Map;
+
 /**
  *
  * @author Sylvia
  */
 public class HuntingController {
 
-    public double calcLengthHunting(double length, double distance, double height) {
-
+    public double calcLengthHunting(double length, double distance, double height)throws HuntingControllerException {
+            Double number = null;
+    while (number == null){
+        String value = this.getInput();
+        value = value.trim().toUpperCase();
+        
         if (height < 55 || height > 66) { // height is incorrect
-            return -1;
+            
         }
         if (distance < 720 || distance > 1080) { // distance is incorrect
-            return -1;
+           
         }
-
+        try{
         length = Math.sqrt(Math.pow(height, 2) - Math.pow(distance, 2));
+       
+        } catch (NumberFormatException nf){
+            System.out.println("\nYou must enter a valid number"
+                    + "Try again or enter Q to Quit");
+        }
+        
+}
+    return number;
+}
 
-        return length;
-
+    private String getInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    public boolean lookBerries(Map mapOne) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean seekHoney(Map mapOne) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean findWheat(Map mapOne) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean searchWater(Map mapOne) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean lookUnknownPlants(Map mapOne) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- lehisJourney/src/byui/cit260/lehisJourney/view/CollectingFoodWaterView.java
+++ lehisJourney/src/byui/cit260/lehisJourney/view/CollectingFoodWaterView.java
@@ -5,7 +5,10 @@
  */
 package byui.cit260.lehisJourney.view;
 
-import java.util.Scanner;
+import byui.cit260.lehisJourney.control.HuntingController;
+import byui.cit260.lehisJourney.exceptions.HuntingControllerException;
+import byui.cit260.lehisJourney.model.Location;
+import lehisjourney.LehisJourney;
 
 /**
  *
@@ -34,19 +37,29 @@ public class CollectingFoodWaterView extends View {
 
         switch (selection) {
             case 'B':
+            {
                 lookBerries();
+            }
                 break;
             case 'H':
+            {
                 seekHoney();
+            }
                 break;
             case 'E':
+            {
                 findWheat();
                 break;
+            }
             case 'W':
+            {
                 searchWater();
+            }
                 break;
             case 'U':
+            {
                 lookUnknownPlants();
+            }
                 break;
             case 'Q':
                 break;
@@ -61,25 +74,48 @@ public class CollectingFoodWaterView extends View {
         CollectingFoodWaterView CollectingFoodWater = new CollectingFoodWaterView();
         CollectingFoodWater.display();
     }
-
+    
     private void lookBerries() {
+       HuntingController hc = new HuntingController();
+        if(!hc.lookBerries(LehisJourney.getMapOne())){
         System.out.println("Look for Berries");
     }
-
+}
     private void seekHoney() {
+        HuntingController hc = new HuntingController();
+        if(hc.seekHoney(LehisJourney.getMapOne())){
         System.out.println("Seek for Honey");
+    }
     }
 
     private void findWheat() {
+       HuntingController hc = new HuntingController();
+        if(hc.findWheat(LehisJourney.getMapOne())){
         System.out.println("Find some Wheat");
     }
-
+    }
     private void searchWater() {
+        HuntingController hc = new HuntingController();
+        if(!hc.searchWater(LehisJourney.getMapOne())){
         System.out.println("Search for Water");
     }
-
+    }
     private void lookUnknownPlants() {
+        HuntingController hc = new HuntingController();
+        if(!hc.lookUnknownPlants(LehisJourney.getMapOne())){
         System.out.println("Look for Unknown Plants");
     }
+    }
+    private void lookForItems() {
+        System.out.println("Look for an item in current location.");
+    }
+private void viewMap() {
+        System.out.println(LehisJourney.getGame().getMap().getMapDisplay());
+    }
 
+    private void currentLocation() {
+        Location loc = LehisJourney.getGame().getPlayer().getLocation();
+        System.out.println("You are at: " + loc.getRow() + ", " + loc.getColumn() + ".");
+        System.out.println("Which is a " + loc.getDescription());
+    }
 }
