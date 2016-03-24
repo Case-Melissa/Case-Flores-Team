f0ae10f4ce69f1c1dd9c412efb58ebe4718e1df9
Melissa Case <Melissa Case@LAPTOP-J102H57U>
2016-03-24 07:52:42 -0700

    Merge origin/master
    Conflicts:
    	lehisJourney/src/byui/cit260/lehisJourney/control/GameControl.java

--- lehisJourney/src/lehisjourney/LehisJourney.java
+++ lehisJourney/src/lehisjourney/LehisJourney.java
@@ -71,7 +71,7 @@ public class LehisJourney {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public void main(String[] args) {
 
         try {
             LehisJourney.inFile = new BufferedReader(new InputStreamReader(System.in));
@@ -81,37 +81,35 @@ public class LehisJourney {
             LehisJourney.logFile = new PrintWriter(filePath);
 
             StartProgramView startView = new StartProgramView();
-            
+
             startView.startProgram();
 
         } catch (Exception e) {
             e.printStackTrace();
-            
-        }
 
-       finally {
+        } finally {
             try {
-            if (LehisJourney.inFile != null) {
-                LehisJourney.inFile.close();
-            }
+                if (LehisJourney.inFile != null) {
+                    LehisJourney.inFile.close();
+                }
 
-            if (LehisJourney.outFile != null) {
-                LehisJourney.outFile.close();
-            }
+                if (LehisJourney.outFile != null) {
+                    LehisJourney.outFile.close();
+                }
 
-            if (LehisJourney.logFile != null) {
-                LehisJourney.logFile.close();
+                if (LehisJourney.logFile != null) {
+                    LehisJourney.logFile.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(LehisJourney.class.getName()).log(Level.SEVERE, null, ex);
+                ErrorView.display(this.getClass().getName(), "main - Error closing files");
+                return;
             }
-        } catch (IOException ex) {
-            Logger.getLogger(LehisJourney.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("main - Error closing files");
-            return;
+            LehisJourney.outFile.close();
         }
-        LehisJourney.outFile.close();
     }
-}
 
-public static Player getPlayer() {
+    public static Player getPlayer() {
         return player;
     }
 
